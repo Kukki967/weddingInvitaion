17bd33c105be50960f89337300516e57cc58ece5
shraddha <shraddhatongya97@gmail.com>
2021-09-24 22:51:01 +0530

    animate

--- js/menikah.js
+++ js/menikah.js
@@ -55,3 +55,28 @@ $(window).load(function() {
   var Body = $("body");
   Body.addClass("preloader-site");
 });
+
+
+const card = document.querySelector(".card");
+const title = document.querySelector(".title");
+const header = document.querySelector(".header img");
+const description = document.querySelector(".info h3");
+
+// Animate In
+
+card.addEventListener("mouseenter", (e) => {
+  card.style.transition = "none";
+  title.style.transform = "translateZ(3px)";
+  header.style.transform = "translateZ(3px) rotateZ(0deg)";
+  description.style.transform = "translateZ(3px)";
+});
+
+// Animate Out
+
+card.addEventListener("mouseleave", (e) => {
+  card.style.transition = "all 0.5s ease";
+  card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+  title.style.transform = "translateZ(0px)";
+  header.style.transform = "translateZ(0px) rotateZ(0deg)";
+  description.style.transform = "translateZ(0px)";
+});
